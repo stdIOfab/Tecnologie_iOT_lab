bcdecbe4569d50b310a2c0417055e6d0ff4d5f1d
stdIOfab <89013212+stdIOfab@users.noreply.github.com>
2023-04-28 10:16:58 +0200

    Update es2.ino
    Aggiunto controllo per l'input con newline al termine

--- Lab2/es2/es2.ino
+++ Lab2/es2/es2.ino
@@ -107,6 +107,9 @@ void checkInput(){
   if(Serial.available() > 0){
     letter = Serial.readString();
     letter.toUpperCase();
+		if(letter.charAt(letter.length()-1)=='\n'){
+			letter = letter.substring(0, letter.length());
+  		}
     if(letter.substring(0, 5).equals("CMIN:") && isFloat(letter.substring(5))){
       check = letter.substring(5).toFloat();
       if(check < coolingMaxTemp && check > heatingMaxTemp){
