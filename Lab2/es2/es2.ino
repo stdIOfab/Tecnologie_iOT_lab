d46f63a0a3f42ec586129d92593563bd82f045e7
stdIOfab <fabio.biscia@gmail.com>
2023-05-01 23:30:27 +0200

    Bug Fixing e nuove funzionalita

--- Lab2/es2/es2.ino
+++ Lab2/es2/es2.ino
@@ -15,7 +15,7 @@ const int TEMPSENSOR_PIN = 14;
 
 //Fan var and const
 const int FAN_PIN = 5;
-const int MIN_SPEED = 70; //valore maggiore di 0 in quanto il motore non riesce a mettersi in rotazione sotto questa soglia
+const int MIN_SPEED = 100; //valore maggiore di 0 in quanto il motore non riesce a mettersi in rotazione sotto questa soglia
 const int MAX_SPEED = 255;//massimo valore PWM
 byte dutyCycle = 0;
 
@@ -33,11 +33,13 @@ unsigned long int last_PIR_presence = 0; //millis() del momento in cui e' stata 
 //Microphone var and const
 const unsigned int SAMPLE_FREQ = 16000; //campionamento a 16 kHz
 const unsigned short N_CHANNELS = 1; //canale mono
-const unsigned int NOISE_TIMEOUT = 10; //minuti
+const unsigned int NOISE_TIMEOUT = 20; //secondi
+const unsigned int SOUND_REFRESH = 500; //ms
 unsigned int sound_threshold = 500; //ampiezza del valore campionato che aziona la presenza
 volatile int n_sound_events = 0; //numero di eventi che appurano la presenza, si parte con 0
 unsigned int timeout_mic = 10; //riferito a minuti
-unsigned long int last_mic_presence = 0; //millis() del momento in cui e' stata rilevata l'ultima presenza mic
+volatile unsigned long int last_mic_presence = 0; //millis() del momento in cui e' stata rilevata l'ultima presenza mic
+unsigned long int last_over_threshold;
 const unsigned int PRESENCE_THRESHOLD = 20; //soglia oltre la quale viene appurata la presenza
 short sampleBuffer[256]; //buffer per i dati PDM provenienti adl microfono
 volatile int samplesRead = 0; //byte letti PDM
@@ -75,7 +77,8 @@ volatile float heatingMaxTemp = 24, heatingMinTemp = 18;  //setpoint HT
 volatile float heating_cooling_delta = 5; //se non vengono rilevate persone abbassa/alza del valore i setpoint mettendosi in modalità risparmio energetico
 unsigned int REFRESH = 1500; //REFRESH dei valori del programma dal loop()
 volatile bool presence = false; //indica se è presente qualcuno a nella stanza
-volatile byte auto_eco = 1; //indica se è attiva la modalita' di risparmio energetica automatica, abilitata di default
+volatile byte auto_eco = 0; //indica se è attiva la modalita' di risparmio energetica automatica, abilitata di default
+volatile byte system_status = 1; //indica se i sistemi di riscaldamento e raffreddamento sono attivi
 
 //funzione per la lettura della temperatura dal sensore, restituisce in float i gradi centigradi
 float readTemp(){
@@ -171,21 +174,36 @@ void checkInput(){
         Serial.println("Intervallo di temperatura non valido. Riprovare.");
       }
     }else if(letter.substring(0, 12).equals("TIMEOUT_PIR:") && isFloat(letter.substring(12))){
-      timeout_PIR = abs(letter.substring(12).toInt());
-      Serial.print("Timeout rilevamento persone impostato a: ");
-      Serial.print(timeout_PIR);
-      Serial.println(" minuti.");
+      check = abs(letter.substring(12).toInt());
+      if(check > 0){
+        timeout_PIR = check;
+        Serial.print("Timeout rilevamento persone tramite sensorre PIR impostato a: ");
+        Serial.print(timeout_PIR);
+        Serial.println(" minuti.");
+      }else{
+        Serial.println("Valore inserito troppo basso. Riprova.");
+      }
     }
     else if(letter.substring(0, 16).equals("SOUND_THRESHOLD:") && isFloat(letter.substring(16))){
-      sound_threshold = abs(letter.substring(16).toInt());
-      Serial.print("Soglia rilevamento suoni ambientali impostata a: ");
-      Serial.println(sound_threshold);
+      check = letter.substring(16).toInt();
+      if(check > 0){
+        sound_threshold = check;
+        Serial.print("Soglia rilevamento suoni ambientali impostata a: ");
+        Serial.println(sound_threshold);
+      }else{
+        Serial.println("Valore inserito troppo basso, riprova.");
+      }
     }
     else if(letter.substring(0, 12).equals("TIMEOUT_MIC:") && isFloat(letter.substring(12))){
-      timeout_mic = abs(letter.substring(12).toInt());
-      Serial.print("Timeout rilevamento persone impostato a: ");
-      Serial.print(timeout_mic);
-      Serial.println(" minuti.");
+      check = letter.substring(12).toInt();
+      if(check >= 2*NOISE_TIMEOUT/60){
+        timeout_mic = check;
+        Serial.print("Timeout rilevamento persone tramite microfono integrato impostato a: ");
+        Serial.print(timeout_mic);
+        Serial.println(" minuti.");
+      }else{
+        Serial.println("Valore inserito troppo basso. Riprova.");
+      }
     }
     else if(letter.substring(0, 9).equals("HC_DELTA:") && isFloat(letter.substring(9))){
       heating_cooling_delta = abs(letter.substring(9).toFloat());
@@ -193,11 +211,24 @@ void checkInput(){
       Serial.println(heating_cooling_delta);
     }
     else if(letter.substring(0, 9).equals("AUTO_ECO:") && isFloat(letter.substring(9))){
-      check = abs(letter.substring(9).toInt());
+      check = letter.substring(9).toInt();
       if(check == 0 || check == 1){
         auto_eco = check;
         Serial.print("Modalita' risparmio energetico: ");
         Serial.println(auto_eco);
+      }else{
+        Serial.println("Modalita' non riconosciuta");
+      }
+    }
+    else if(letter.substring(0, 11).equals("SYS_STATUS:") && isFloat(letter.substring(11))){
+      check = letter.substring(11).toInt();
+      if(check == 1){
+        system_status = check;
+        Serial.println("Sistema attivato.");
+      }
+      else if(check == 0){
+        system_status = check;
+        Serial.println("Sistema disattivato.");
       }else{
         Serial.println("Modalita' non riconosciuta");
       }
@@ -210,15 +241,19 @@ void checkInput(){
 }
 
 //aggiorna la variabile inserendo quando è stato rilevato l'evento
-void presenceDetected(){
+void PIRpresenceDetected(){
   last_PIR_presence = millis();
+}
+
+void MICpresenceDetected(){
+  last_mic_presence = millis();
 }
 
 //attraverso i vari timeout del mic e del PIR aggiorna costantemente la variabile presence per determinare se è presente qualcuno in casa
 void checkPresence(){
   if((((millis() - last_PIR_presence) < (60000*timeout_PIR) && last_PIR_presence != 0) || ((millis() - last_mic_presence) < (60000*timeout_mic) && last_mic_presence != 0))){
     presence = true;
-  }else if(auto_eco == 1){
+  }else{
     presence = false;
   }
   yield();
@@ -227,12 +262,12 @@ void checkPresence(){
 //attiva o disattiva la ventola AC
 void checkCooling(){
   int real_delta = 0;
-  if(!presence){
+  if(!presence && auto_eco == 1){
     real_delta = heating_cooling_delta;
   }
-  if(readTemp() >= coolingMinTemp+real_delta && readTemp() <= coolingMaxTemp+real_delta){
+  if(readTemp() >= coolingMinTemp+real_delta && readTemp() <= coolingMaxTemp+real_delta && system_status == 1){
     dutyCycle = mapFloat(readTemp(), coolingMinTemp+real_delta, coolingMaxTemp+real_delta, MIN_SPEED, MAX_SPEED);
-  }else if(readTemp() > coolingMaxTemp+real_delta){
+  }else if(readTemp() > coolingMaxTemp+real_delta && system_status == 1){
     dutyCycle = MAX_SPEED;
   }else{
     dutyCycle = 0;
@@ -243,15 +278,15 @@ void checkCooling(){
 //attiva o disattiva il sistema HT
 void checkHeating(){
   int real_delta = 0;
-  if(!presence){
+  if(!presence && auto_eco == 1){
     real_delta = heating_cooling_delta;
   }
-  if(readTemp() >= heatingMinTemp-real_delta && readTemp() <= heatingMaxTemp-real_delta){
+  if(readTemp() >= heatingMinTemp-real_delta && readTemp() <= heatingMaxTemp-real_delta && system_status == 1){
     brightness = mapFloat(readTemp(), heatingMinTemp-real_delta, heatingMaxTemp-real_delta, MAX_BRIGHTNESS, MIN_BRIGHTNESS);
-  }else if(readTemp() > heatingMaxTemp-real_delta){
-    brightness = MIN_BRIGHTNESS;
-  }else{
+  }else if(readTemp() < heatingMinTemp-real_delta && system_status == 1){
     brightness = MAX_BRIGHTNESS;
+  }else{
+    brightness = MIN_BRIGHTNESS;
   }
   analogWrite(RLED_PIN, brightness);
 }
@@ -267,28 +302,30 @@ void checkSound(){
   if(samplesRead != 0){
     for(int i=0; i < samplesRead; i++){
       if(abs(sampleBuffer[i]) > sound_threshold){
+        last_over_threshold = millis();
         n_sound_events++;
-        last_mic_presence = millis();
         break;
         }       
     }
-    if(millis() - last_mic_presence < 60000*NOISE_TIMEOUT){
+    if(millis() - last_over_threshold < 1000*NOISE_TIMEOUT){
       if(n_sound_events > PRESENCE_THRESHOLD){
-        presenceDetected();
+        MICpresenceDetected();
       }
     }else{
       n_sound_events = 0;
     }
     samplesRead = 0;
   }
+  yield();
+  delay(SOUND_REFRESH);
 }
 
 //stampa del menu iniziale sul seriale
 void printMenu(){
-  char buffer[300];
+  char buffer[500];
   Serial.println("Guida ai comandi del Controller Smart Home:");
-  sprintf(buffer, "Il controller gestisce le periferiche di AC e HT della casa. E' presente una lista di comandi attraverso \n i quali e' possibile modificare il comportamento del sistema. Ricorda che i setpoint sono soggetti ad un delta di risparmio energetico \n nel caso in cui questa modalita' sia attivata. In tal caso i display visualizzeranno il valore attuale tenendo conto del delta, tuttavia il setpoint originale rimarra' invariato.\n");
-  Serial.print(buffer);
+  sprintf(buffer, "Il controller gestisce le periferiche di AC e HT della casa. E' presente una lista di comandi \n attraverso i quali e' possibile modificare il comportamento del sistema. \n Ricorda che i setpoint sono soggetti ad un delta di risparmio energetico nel caso in cui \n questa modalita' sia attivata. In tal caso i display visualizzeranno il valore attuale \n tenendo conto del delta, tuttavia il setpoint originale rimarra' invariato.");
+  Serial.println(buffer);
   Serial.println("  Parametri modificabili:");
   Serial.println("    - CMIN:<+/-VALUE>: modifica la temperatura minima di raffreddamento.");
   Serial.println("    - CMAX:<+/-VALUE>: modifica la temperatura massima di raffreddamento.");
@@ -297,22 +334,23 @@ void printMenu(){
   Serial.println("    - TIMEOUT_PIR:<+/-VALUE>: modifica il periodo dopo il quale, senza ulteriori movimenti, la casa viene considerata vuota.");
   Serial.println("    - TIMEOUT_MIC:<+/-VALUE>: modifica il periodo dopo il quale, senza ulteriori suoni, la casa viene considerata vuota.");
   Serial.println("    - SOUND_THRESHOLD:<+/-VALUE>: modifica la soglia di rumore per la quale il sistema rileva la presenza.");
-  Serial.println("    - AUTO_ECO:<0/1>: disattiva o attiva il risparmio energetico automatico, abilitato di default");
+  Serial.println("    - AUTO_ECO:<0/1>: disattiva o attiva il risparmio energetico automatico, disabilitata di default");
+  Serial.println("    - SYS_STATUS:<0/1>: disattiva o attiva l'intero sistema, abilitato di default");
 }
 
 //stampa sul display LCD dei valori di temperatura, intensita sistemi AC e HT, e setpoint
 void printLCD(){
   int real_delta = 0;
-  if(!presence){
+  if(!presence && auto_eco == 1){
     real_delta = heating_cooling_delta;
   }
   char charBuffer[16];
   lcd.clear();
   lcd.setCursor(0, 0);
-  sprintf(charBuffer, "T:%.1f C <\02>:%d <\01>:%d", readTemp(), presence, auto_eco);
+  sprintf(charBuffer, "T:%.1f\C \02:%d \01:%d", readTemp(), presence, auto_eco);
   lcd.print(charBuffer);
   lcd.setCursor(0, 1);
-  sprintf(charBuffer, "AC:%.1f\%% HT:%.1f\%%", mapFloat(dutyCycle, 0, MAX_SPEED, 0, 100), mapFloat(brightness, 0, MAX_BRIGHTNESS, 0, 100));
+  sprintf(charBuffer, "AC:%.0f\%% HT:%.0f\%%", (dutyCycle < MIN_SPEED)? 0 : mapFloat(dutyCycle, MIN_SPEED, MAX_SPEED, 0, 100), (brightness < MIN_BRIGHTNESS)? 0 : mapFloat(brightness, MIN_BRIGHTNESS, MAX_BRIGHTNESS, 0, 100));
   lcd.print(charBuffer);
   delay(LCD_DELAY);
   lcd.clear();
@@ -323,6 +361,7 @@ void printLCD(){
   sprintf(charBuffer, "HT m:%.1f M:%.1f", heatingMinTemp-real_delta, heatingMaxTemp-real_delta);
   lcd.print(charBuffer);
   delay(LCD_DELAY);
+  yield();
   }
 
 void setup() {
@@ -342,8 +381,6 @@ void setup() {
 
   printMenu();
 
-  lcd.createChar(1, ecoChar);
-  lcd.createChar(2, presenceChar);
   while(LCD_ready!=0){
   Wire.begin();
   Wire.beginTransmission(0xA0);
@@ -357,13 +394,15 @@ void setup() {
   lcd.print("Monitor avviato");
   lcd.setCursor(0, 1);
   lcd.print("correttamente...");
+  lcd.createChar(1, ecoChar);
+  lcd.createChar(2, presenceChar);
   delay(2000);
   
   Scheduler.startLoop(checkInput);
   Scheduler.startLoop(checkPresence);
   Scheduler.startLoop(printLCD);
   Scheduler.startLoop(checkSound);
-  attachInterrupt(digitalPinToInterrupt(PIR_PIN), presenceDetected, RISING);
+  attachInterrupt(digitalPinToInterrupt(PIR_PIN), PIRpresenceDetected, RISING);
   PDM.onReceive(onPDMdata);
 }
 
